0edd18336f0c0a46abf26ad825f19846c54e4a70
aunsuthawee <aunderu@hotmail.com>
2023-01-30 13:46:18 +0700

    Update screens.dart

--- lib/screens.dart
+++ lib/screens.dart
@@ -4,7 +4,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:rac_road/loading/skelton.dart';
 import 'package:rac_road/models/user_profile_model.dart';
 import 'package:rac_road/services/remote_service.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/my_job_models.dart';
 import '../models/current_tnc_sos_models.dart';
